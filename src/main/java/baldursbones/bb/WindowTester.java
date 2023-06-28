21bbe0e549b21f9038fb82cca158bb162a69c8ab
Braden Rogers <brogers42@my.bcit.ca>
2023-06-28 13:32:30 -0700

    Set size of window to near fullscreen and added screen size scaling

--- src/main/java/baldursbones/bb/WindowTester.java
+++ src/main/java/baldursbones/bb/WindowTester.java
@@ -2,7 +2,9 @@ package baldursbones.bb;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +13,11 @@ public class WindowTester extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(WindowTester.class.getResource("test-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Rectangle2D userScreen = Screen.getPrimary().getBounds();
+        Scene scene = new Scene(fxmlLoader.load(), userScreen.getWidth(), userScreen.getHeight() - 70);
+        stage.setTitle("Main Menu");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
