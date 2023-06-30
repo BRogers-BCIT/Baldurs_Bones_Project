2a251fc81e48d252feb0fdb5d27a307d141afac4
Braden Rogers <brogers42@my.bcit.ca>
2023-06-30 11:32:20 -0700

    Finished spacing rules for save window table

--- src/main/java/baldursbones/bb/SavesWindowController.java
+++ src/main/java/baldursbones/bb/SavesWindowController.java
@@ -1,28 +1,47 @@
 package baldursbones.bb;
 
+import eu.hansolo.tilesfx.Test;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SavesWindowController implements Initializable {
 
-    @FXML private TableView saves;
+    @FXML private TableView<TestSaves> saves;
 
-    @FXML private TableColumn saveName;
+    @FXML private TableColumn<TestSaves, String> saveName;
 
-    @FXML private TableColumn characterName;
+    @FXML private TableColumn<TestSaves, String> characterName;
 
-    @FXML private TableColumn saveTime;
+    @FXML private TableColumn<TestSaves, String> saveTime;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("test");
         saveName.minWidthProperty().bind(saves.widthProperty().divide(3));
+        saveName.setCellValueFactory(new PropertyValueFactory<>("saveName"));
         characterName.minWidthProperty().bind(saves.widthProperty().divide(3));
+        characterName.setCellValueFactory(new PropertyValueFactory<> ("characterName"));
         saveTime.minWidthProperty().bind(saves.widthProperty().divide(3));
+        saveTime.setCellValueFactory(new PropertyValueFactory<> ("saveTime"));
+
+        saves.setItems(getSaves());
+    }
+
+    public ObservableList<TestSaves> getSaves() {
+        ObservableList<TestSaves> saves = FXCollections.observableArrayList();
+        saves.add(new TestSaves("Save 1", "Will", "5 days ago"));
+        saves.add(new TestSaves("Save 2", "Bill", "4 days ago"));
+        saves.add(new TestSaves("Save 3", "Ned", "3 days ago"));
+        saves.add(new TestSaves("Save 4", "Ted", "2 days ago"));
+        saves.add(new TestSaves("Save 5", "Edd", "1 days ago"));
+        return saves;
     }
 }
