0d8a83891accba396a21462674f4d14363812825
Braden Rogers <brogers42@my.bcit.ca>
2023-06-30 13:49:50 -0700

    Created tester for windows in either default, specific size, or fullscreen sizing. Resizing enabled.

--- src/main/java/baldursbones/bb/WindowTester.java
+++ src/main/java/baldursbones/bb/WindowTester.java
@@ -6,18 +6,28 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class WindowTester extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(WindowTester.class.getResource("MainMenu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(WindowTester.class.getResource("FileName.fxml"));
         Rectangle2D userScreen = Screen.getPrimary().getBounds();
+
+        // Fullscreen sizing
         Scene scene = new Scene(fxmlLoader.load(), userScreen.getWidth(), userScreen.getHeight() - 70);
-        stage.setTitle("Main Menu");
+
+        // Set sizing
+        //Scene scene = new Scene(fxmlLoader.load(), X, Y);
+
+        // Default sizing
+        //Scene scene = new Scene(fxmlLoader.load());
+
+        // Turn off resizing
+        // stage.setResizable(false);
+
+        stage.setTitle("Test Screen");
         stage.setScene(scene);
-        stage.setResizable(false);
         stage.show();
     }
 
