575091ec55828bdc7d3f2f42090962f4725380d8
Braden Rogers <brogers42@my.bcit.ca>
2023-07-05 11:37:46 -0700

    Updated public documentation for methods and class fields. Added private documentation on method workings. Simplified print statements for testing.

--- src/main/java/baldursbones/bb/BossCombat.java
+++ src/main/java/baldursbones/bb/BossCombat.java
@@ -2,54 +2,79 @@ package baldursbones.bb;
 
 import java.util.Scanner;
 
-/** Boss Combat.
+/** Boss Fight Combat Extention.
  * @author Braden Rogers
- * @version 2023-TermProject
+ * @version Baldur's Bones v1.1
  */
 public class BossCombat extends Combat {
+    // Constant: maximum number of rounds of combat against boss.
     private static final int MAX_ROUNDS = 3;
-    /** The number of rounds the player has lost.
+
+    /** Class Value: Record the number of rounds the boss has won against the player.
      */
     protected int roundsWon;
-    /** The number of rounds the player has won.
+
+    /** Class Value: Record the number of rounds the boss has lost to the player.
      */
     protected int roundsLost;
+
+    // Counter: Records the current round of combat. Min = 1, Max = Max_Rounds.
     private int rounds;
-    /** Creates a new combat class with the player and the passed boss enemy.
-     * @param player the players character object
-     * @param newEnemy the boss object the player should play against
+
+
+    /** Create a new combat class for a boss fight combat. Takes a passed player object and a new boss enemy object.
+     * @param player the current character object for the player
+     * @param newBoss a new boss type enemy object
      */
-    public BossCombat(final Player player, final BossEnemy newEnemy) {
-        super(player, newEnemy);
+    public BossCombat(final Player player, final BossEnemy newBoss) {
+        super(player, newBoss);
         rounds = 0;
         roundsWon = 0;
         roundsLost = 0;
     }
 
-    /** Starts the combat beginning of the first combat round and returns outcome when finished.
-     * @return the integer outcome value of the combat
+    /** Informs the player of their current stats and calls the combat loop to start the beginning of the combat.
+     * Returns the outcome of the round when finished (-2 = loss, +2 = win).
+     * @return An integer that represents the outcome of the combat
      */
     @Override
     public int startCombat() {
-        System.out.println("You have begun a game of Baldur's Bones with the adventurer Volo.");
+        Scanner scan = new Scanner(System.in);
+
+        // Print the players stats and prompt the player to continue.
+        System.out.println("Begin final boss fight.");
         System.out.println("Your current stats are: ");
         pc.getStats();
-        Scanner scan = new Scanner(System.in);
         System.out.println("Enter to continue");
         scan.nextLine();
+
+        // Call the combat loop method
         combatLoop();
+
+        // Find the final outcome of the combat and return it (-2 = loss, +2 = win).
         setOutcome();
         return outcome;
     }
+
+    // Begins a while loop to repeat combat rounds in a best-of-three.
     private void combatLoop() {
+        // While less than 3 rounds have been player and neither the player has won two rounds.
         while (rounds < MAX_ROUNDS && roundsWon < 2 && roundsLost < 2) {
-            System.out.println("You start a new round with Volo. It is currently round " + (rounds + 1) + ".");
-            System.out.println("You have won " + roundsWon + " rounds against Volo.");
-            System.out.println("You have lost " + roundsLost + " rounds against Volo.");
+            // Inform player of current round status.
+            System.out.println("Start new round. Current round: " + (rounds + 1) + ".");
+            System.out.println("Rounds won: " + roundsWon + ".");
+            System.out.println("Rounds lost: " + roundsLost + ".");
+
+            // Call new starting rolls and invoke the player combat loop.
             startRoll();
             playerChoice = 1;
             super.playerChoose();
-            outcome = enemy.compareRoll(playerRoll);
+
+            // Get the bosses roll for the round and compare to the player.
+            // (+1 = player win, -1 = player loss.)
+            outcome = enemy.compareRoll(playerTotal);
+
+            // Compare boss and player rolls and increment the rounds won or lost.
             if (outcome > 0) {
                 roundsWon += 1;
                 ((BossEnemy) enemy).winRound();
@@ -57,6 +82,8 @@ public class BossCombat extends Combat {
                 roundsLost += 1;
                 ((BossEnemy) enemy).loseRound();
             }
+
+            // Increment the number of rounds fought and prompt player to continue.
             rounds += 1;
             Scanner scan = new Scanner(System.in);
             System.out.println("Enter to continue \n");
@@ -64,16 +91,19 @@ public class BossCombat extends Combat {
         }
     }
 
-    /** Sets the outcome of the fight based on the number of rounds won or lost.
+    /** Set the outcome value based on the number of rounds won or lost.
      */
     protected void setOutcome() {
+        // If rounds won == 2 then the player wins and sets the outcome to 2.
         if (roundsWon == 2) {
             outcome = 0;
             outcome += 2;
         }
+        // If rounds lost == 2 then the player loses and sets the outcome to -2.
         if (roundsLost == 2) {
             outcome = 0;
             outcome -= 2;
         }
+        // If neither the boss nor the player has won 2 rounds then do not set the outcome.
     }
 }
--- src/main/java/baldursbones/bb/Combat.java
+++ src/main/java/baldursbones/bb/Combat.java
@@ -5,72 +5,98 @@ import java.util.Scanner;
 
 /** Combat.
  * @author Braden Rogers
- * @version 2023-TermProject
+ * @version Baldur's Bones v1.1
  */
 public class Combat {
-   /** The maximum (exclusive) value for a roll.
-   */
+
+   /** Constant: The maximum (exclusive) value for a roll. Limit rolls to 1 - 6.
+    */
    protected static final int ROLL_MAX = 7;
-   /** The total value at which a player will always lose.
+
+   /** Constant: The highest valid value in a round of combat. Exceeding Total_Bust will result in the player losing.
     */
    protected static final int TOTAL_BUST = 21;
-   /** A value from which a player can never win. Prevents infinite rolling.
+
+
+   /** Constant: a ceiling value to stop player from rolling forever (No use of abilities can result in valid total).
     */
    protected static final int TOTAL_MAX = 30;
-   /** The integer value for the roll choice for a player.
+
+   /** Constant: The value used to indicate a player choice of adding a new roll.
     */
    protected static final int ROLL = 1;
-   /** The integer value for the re-roll choice for a player.
+
+   /** Constant: The value used to indicate a player choice of holding their current total.
     */
-   protected static final int RE_ROLL = 2;
-   /** The integer value for the add choice for a player.
+   protected static final int HOLD = 2;
+
+   /** Constant: The value used to indicate a player choice of adding one to their total.
     */
    protected static final int ADD = 3;
-   /** The integer value for the take-away choice for a player.
+
+   /** Constant: The value used to indicate a player choice of subtracting one from their total.
     */
    protected static final int TAKE_AWAY = 4;
-   /** The player object representing the player character.
+
+    /** Constant: The value used to indicate a player choice of re-rolling their last roll.
+     */
+    protected static final int RE_ROLL = 5;
+
+   /** Object: The player object representing current the player character.
    */
    protected final Player pc;
-   /** The enemy object representing the enemy being fought.
+
+   /** Object: The enemy object representing the current enemy being fought.
     */
    protected final Enemy enemy;
-   /** The players current roll total.
+
+   /** Variable: The players current roll total.
     */
-   protected int playerRoll;
-   /** The players last current roll value.
+   protected int playerTotal;
+
+   /** Variable: The value of the players last die rolled by the player.
     */
    protected int lastRoll;
-   /** The players current choice value.
+
+   /** The value that records what the player choose to do.
     */
    protected int playerChoice;
-   /** The outcome value of the combat.
+
+   /** The value used to record the outcome of the combat (-1 = player loses, 1 = player wins).
     */
    protected int outcome;
-    /** Creates a new combat class with the player and the passed enemy.
-     * @param player the players character object
-     * @param newEnemy the enemy object the player should play against
-     */
+
+   /** Create a new combat and sets initial values.
+    * Takes a player character object and a new enemy object.
+    * @param player the current player character object
+    * @param newEnemy a new enemy object for the player to play against
+    */
    public Combat(final Player player, final Enemy newEnemy) {
        pc = player;
        enemy = newEnemy;
-       playerRoll = 0;
+       playerTotal = 0;
        playerChoice = 1;
        outcome = 0;
    }
 
-    /** Starts the combat cycle and returns outcome when finished.
-     * @return the integer outcome value of the combat
+    /** Print the player stats, get the starting rolls and call the game choice loop.
+     * When the loop ends call the combat end method and return the outcome.
+     * @return an integer value used to represent the outcome of the combat
      */
    public int startCombat() {
-       System.out.println("You have begun a game of Baldur's Bones.");
-       System.out.println("Your current stats are as follows.");
-       pc.getStats();
+       // Print current player stats and prompt player to continue.
        Scanner scan = new Scanner(System.in);
+       System.out.println("Begin Combat.");
+       System.out.println("Current Stats: ");
+       pc.getStats();
        System.out.println("Enter to continue");
        scan.nextLine();
+
+       // Get the starting rolls for the player and call the game choice loop.
        startRoll();
        playerChoose();
+
+       // Get the outcome of the combat and return it.
        finishGame();
        return outcome;
    }
@@ -78,112 +104,137 @@ public class Combat {
     /** Sets the players roll to its starting value and informs them of their rolls.
      */
    protected void startRoll() {
-       playerRoll = 0;
+       // Set the starting total to 0 and print the players starting rolls.
+       playerTotal = 0;
+       System.out.println("Starting rolls:");
        Random rand = new Random();
-       System.out.println("To start the game you roll:");
+
+       // Generate the first starting roll, print it and add it to the total.
        lastRoll = rand.nextInt(1, ROLL_MAX);
        System.out.println(lastRoll);
-       playerRoll += lastRoll;
+       playerTotal += lastRoll;
+
+       // Generate the second starting roll, print it and add it to the total.
        lastRoll = rand.nextInt(1, ROLL_MAX);
        System.out.println(lastRoll);
-       playerRoll += lastRoll;
+       playerTotal += lastRoll;
+
+       // Generate the final starting roll, print it, add it to the total, and save it as the last player roll.
        lastRoll = rand.nextInt(1, ROLL_MAX);
        System.out.println(lastRoll);
-       playerRoll += lastRoll;
+       playerTotal += lastRoll;
    }
 
     /** Creates loop to allow player to take game actions until they quit or cannot win.
      */
    protected void playerChoose() {
        Scanner scan = new Scanner(System.in);
-       while (playerChoice != 0 && playerRoll < TOTAL_MAX) {
-           System.out.println();
-           if (playerRoll > TOTAL_BUST) {
-               System.out.println("Your current roll is over 21. "
-                       + "You will lose if you do not use a cheat to reduce your total.");
+       // Loop through player options while player total is not above max roll and last choice was not hold total.
+       while (playerChoice != 2 && playerTotal < TOTAL_MAX) {
+           if (playerTotal > TOTAL_BUST) {
+               System.out.println("Current roll exceeds 21.");
            }
-           System.out.println("Your current roll total is: " + playerRoll);
-           System.out.println("Your last roll was: " + lastRoll);
-           System.out.println("Select the move you want to make");
-           System.out.println("1 - Roll, 2 - ReRoll");
-            if (pc.getLevel() > 1) {
-                System.out.println("3 - Add, 4 - Take Away");
-            }
-            System.out.println("0 - Hold");
-            playerChoice = scan.nextInt();
-            if (playerChoice != 0) {
-                playerChoiceDirector();
-            }
+
+           // Print current combat values
+           System.out.println("Current total: " + playerTotal);
+           System.out.println("Last Roll: " + lastRoll);
+           System.out.println("Select move: ");
+
+           // Print player choices
+           System.out.println("1 - Roll, 2 - Hold");
+           if (pc.getLevel() > 1) {
+                System.out.println("3 - Add, 4 - Take Away, 5 - Re-Roll");
+           }
+
+           // Record player choices and call response printer.
+           playerChoice = scan.nextInt();
+           if (playerChoice != HOLD) {
+               playerChoiceDirector();
+           }
        }
-       System.out.println("Your ending rolled total was " + playerRoll + ".");
-       if (playerRoll > TOTAL_BUST) {
+
+       // Print the players final total and check if they lose to bust.
+       System.out.println("Your end total is " + playerTotal + ".");
+       if (playerTotal > TOTAL_BUST) {
            playerBust();
        }
    }
-    private void playerBust() {
-       System.out.println("You ended the round with a roll higher than 21.");
-       System.out.println("You will lose the current round.");
-       playerRoll = 0;
-    }
-    /** Calls the game action method based on the player choice value.
-     */
+
+   // If the player ended with a roll > 21, they automatically lose (set total to 0).
+   private void playerBust() {
+      System.out.println("Ended with total greater than 21. You will lose.");
+      playerTotal = 0;
+   }
+
+   /** Call a game action method based on the player choice value.
+    * If there is not a valid choice inform player and return.
+    */
    protected void playerChoiceDirector() {
+       // Call method based on player choice:
+       // 1 = Add Roll, 2 = Hold (N/A), 3 = Add to total, 4 = Subtract from total, 5 = Re-Roll last roll.
        if (playerChoice == ROLL) {
            playerRoll();
-       } else if (playerChoice == RE_ROLL) {
-           playerReRoll();
        } else if (playerChoice == ADD && pc.getLevel() > 1) {
            playerAdd();
        } else if (playerChoice == TAKE_AWAY && pc.getLevel() > 1) {
            playerTakeAway();
+       } else if (playerChoice == RE_ROLL && pc.getLevel() > 1) {
+           playerReRoll();
+
+       // If no valid option was chosen print warning and return.
        } else if (playerChoice != 0) {
-           System.out.println("\n That is an invalid choice! \n");
+           System.out.println("Invalid choice.");
        }
    }
 
-    /** Gets the enemy roll and compares it to the player, then sets the outcome.
+    /** Call the getRoll method from the enemy to get enemy roll.
+     * Compare against player roll and set the outcome (true = player wins, false = player loses)
      */
    protected void finishGame() {
-       outcome = enemy.compareRoll(playerRoll);
+       outcome = enemy.compareRoll(playerTotal);
    }
 
-    /** Makes a new roll and adds it to the players total.
+    /** Generate a new roll value between 1-6.
+     * Add it to total, set last roll to its value and inform player of roll.
      */
    protected void playerRoll() {
        Random rand = new Random();
        lastRoll = rand.nextInt(1, ROLL_MAX);
-       playerRoll += lastRoll;
-       System.out.println("You rolled a " + lastRoll + ".");
+       playerTotal += lastRoll;
+       System.out.println("Rolled: " + lastRoll + ".");
    }
 
-    /** Removes the last roll from player total and add a new roll to the player total.
+    /** Subtract the last roll value from player total and add a new roll to the player total.
      */
    protected void playerReRoll() {
+       // Call use Re-Roll method to check if the ability can be used.
        if (pc.useReRoll()) {
-           System.out.println("You removed " + lastRoll + ".");
-           playerRoll -= lastRoll;
+           // Remove the last roll from the total.
+           System.out.println("Removed: " + lastRoll + ".");
+           playerTotal -= lastRoll;
+           // Generate new roll, add it to total, and record it as the last roll.
            Random rand = new Random();
            lastRoll = rand.nextInt(1, ROLL_MAX);
-           playerRoll += lastRoll;
-           System.out.println("You rolled a " + lastRoll + ".");
+           playerTotal += lastRoll;
+           System.out.println("Added: " + lastRoll + ".");
        }
    }
 
-    /** Adds 1 to the players total.
+    /** Add one to the current total value.
      */
     protected void playerAdd() {
         if (pc.useAdd()) {
-            playerRoll += 1;
-            System.out.println("You added 1 to your total roll.");
+            playerTotal += 1;
+            System.out.println("Added 1.");
         }
    }
 
-    /** Take 1 away from the players total.
+    /** Subtract one from the current total value.
      */
    protected void playerTakeAway() {
        if (pc.useTakeAway()) {
-           playerRoll -= 1;
-           System.out.println("You removed 1 from your total roll.");
+           playerTotal -= 1;
+           System.out.println("Subtracted 1.");
        }
    }
 }
