a3e1d8e678bd9fed6647e19f8ab7717e2f9180a3
Braden Rogers <brogers42@my.bcit.ca>
2023-07-06 11:09:32 -0700

    Replaced dialogue print lines with text file reader and added file variable.

--- src/main/java/baldursbones/bb/TutorialEnemy.java
+++ src/main/java/baldursbones/bb/TutorialEnemy.java
@@ -1,8 +1,12 @@
 package baldursbones.bb;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/** Tutorial Enemy Implementation.
+/**
+ * Tutorial Enemy Implementation.
+ *
  * @author Braden Rogers
  * @version Baldur's Bones v1.1
  */
@@ -11,7 +15,12 @@ public class TutorialEnemy extends Enemy {
     // Constant: Defines the static difficulty of the enemy.
     private static final int DIFFICULTY = 14;
 
-    /** Create a Tutorial difficulty implementation of the Enemy Abstract.
+    // Text file: contains all dialogue to be printed by the tutorial enemy class.
+    private final File tutorialEnemyText
+            = new File("src/main/resources/baldursbones/bb/TutorialEnemyText.txt");
+
+    /**
+     * Create a Tutorial difficulty implementation of the Enemy Abstract.
      */
     public TutorialEnemy() {
         super();
@@ -19,8 +28,10 @@ public class TutorialEnemy extends Enemy {
         enemyTotal = DIFFICULTY;
     }
 
-    /** Generates a "total" value for the enemy and compares the value to the passed player "total".
+    /**
+     * Generates a "total" value for the enemy and compares the value to the passed player "total".
      * Regardless of outcome return a 0 (Tutorial win or loss does not affect player).
+     *
      * @param playerRoll an integer value representing the users end total
      * @return an integer representing the outcome of the game (0 = finished tutorial)
      */
@@ -39,23 +50,63 @@ public class TutorialEnemy extends Enemy {
         return 0;
     }
 
-    /** Define the enemy behavior (Game end text) if the player loses.
+    /**
+     * Define the enemy behavior (Game end text) if the player loses.
+     *
+     * @throws RuntimeException if text file is missing
      */
     protected void win() {
-        System.out.println("Tutorial loss.");
-        tutorialEnd();
-    }
-    /** Define the enemy behavior (Game end text) if the player loses.
-     */
-    protected void lose() {
-        System.out.println("Tutorial Win.");
+        // Try to read the win tutorial combat text from the TutorialEnemy text file.
+        try {
+            Scanner fileReader = new Scanner(tutorialEnemyText);
+            // Print the text to user.
+            System.out.println(fileReader.nextLine());
+        } catch (FileNotFoundException e) {
+            // Catch any errors with reading the text file.
+            throw new RuntimeException(e);
+        }
         tutorialEnd();
     }
 
-    /** Displays the game text for when tutorial finishes regardless of win or lose.
+    /**
+     * Define the enemy behavior (Game end text) if the player loses.
+     *
+     * @throws RuntimeException if text file is missing
+     */
+    protected void lose() {
+        // Try to read the lose tutorial combat text from the TutorialEnemy text file.
+        try {
+            Scanner fileReader = new Scanner(tutorialEnemyText);
+            // Skip the first line of text.
+            fileReader.nextLine();
+            // Print the text to user.
+            System.out.println(fileReader.nextLine());
+        } catch (FileNotFoundException e) {
+            // Catch any errors with reading the text file.
+            throw new RuntimeException(e);
+        }
+        tutorialEnd();
+    }
+
+    /**
+     * Displays the game text for when tutorial finishes regardless of win or lose.
+     *
+     * @throws RuntimeException if text file is missing
      */
     protected void tutorialEnd() {
-        System.out.println("Tutorial end and game description.");
+        // Try to read the end tutorial text from the TutorialEnemy text file.
+        try {
+            Scanner fileReader = new Scanner(tutorialEnemyText);
+            // Skip the first two lines of text.
+            fileReader.nextLine();
+            fileReader.nextLine();
+            // Print the text to user.
+            System.out.println(fileReader.nextLine());
+        } catch (FileNotFoundException e) {
+            // Catch any errors with reading the text file.
+            throw new RuntimeException(e);
+        }
+        // Prompt the user to continue. ** Replace with button press. **
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter to continue");
         scan.nextLine();
--- src/main/java/baldursbones/bb/TutorialLocation.java
+++ src/main/java/baldursbones/bb/TutorialLocation.java
@@ -1,8 +1,12 @@
 package baldursbones.bb;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/** Tutorial Location Implementation.
+/**
+ * Tutorial Location Implementation.
+ *
  * @author Braden Rogers
  * @version Baldur's Bones v1.1
  */
@@ -11,42 +15,74 @@ public class TutorialLocation extends Location {
     // Location value for returning to tutorial location.
     private static final int RETURNED_TUTORIAL = 1;
 
-    /** Creates a new location object and assigns it a location value.
+    // Text file: contains all dialogue to be printed by the tutorial location class.
+    private final File tutorialLocationText
+            = new File("src/main/resources/baldursbones/bb/TutorialLocationText.txt");
+
+    /**
+     * Creates a new location object and assigns it a location value.
+     *
      * @param newLocationType an integer representing the location's value.
      */
     public TutorialLocation(final int newLocationType) {
         super(newLocationType);
     }
 
-    /** Gets the description of the tutorial location based on its location value.
+    /**
+     * Gets the description of the tutorial location based on its location value.
+     *
      * @return a boolean indicating if the current location is a combat location
      */
     public boolean getDescription() {
         if (locationType == RETURNED_TUTORIAL) {
-            return  fightLocation();
+            return fightLocation();
         } else {
             exploreLocation();
             return false;
         }
     }
 
-    /** Prints the text for the start of the tutorial and leads into the tutorial fight.
+    /**
+     * Prints the text for the start of the tutorial and leads into the tutorial fight.
+     *
      * @return a boolean value indicating if a fight can be started at this location
+     * @throws RuntimeException if text file is missing
      */
     protected boolean fightLocation() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Tutorial introduction.");
-        // Prompt user to continue.
+        // Try to read the start tutorial text from the TutorialLocation text file.
+        try {
+            Scanner fileReader = new Scanner(tutorialLocationText);
+            // Print the text to user.
+            System.out.println(fileReader.nextLine());
+        } catch (FileNotFoundException e) {
+            // Catch any errors with reading the text file.
+            throw new RuntimeException(e);
+        }
+        // Prompt the user to continue. ** Replace with button press. **
         System.out.println("Enter to continue");
         scan.nextLine();
         return true;
     }
 
-    /** Prints the text for returning to the tutorial location.
+    /**
+     * Prints the text for returning to the tutorial location.
+     *
+     * @throws RuntimeException if text file is missing
      */
     protected void exploreLocation() {
-        System.out.println("Return to tutorial location text.");
-        // Prompt user to continue.
+        // Try to read the return to tutorial text from the TutorialLocation text file.
+        try {
+            Scanner fileReader = new Scanner(tutorialLocationText);
+            // Skip the first line of text.
+            fileReader.nextLine();
+            // Print the text to user.
+            System.out.println(fileReader.nextLine());
+        } catch (FileNotFoundException e) {
+            // Catch any errors with reading the text file.
+            throw new RuntimeException(e);
+        }
+        // Prompt the user to continue. ** Replace with button press. **
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter to continue");
         scan.nextLine();
