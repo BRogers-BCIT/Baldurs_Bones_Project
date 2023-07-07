9ed71f0c043dec376516b908f3255c369f2b09e8
Braden Rogers <brogers42@my.bcit.ca>
2023-07-07 14:58:59 -0700

    Added test code for having one FXML document display inside another.

--- src/main/java/baldursbones/bb/MainMenuController.java
+++ src/main/java/baldursbones/bb/MainMenuController.java
@@ -7,9 +7,9 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class MainMenuController {
@@ -21,18 +21,17 @@ public class MainMenuController {
     private Parent root;
 
     @FXML
+    private GridPane MainMenuGrid;
+
+    @FXML
     public void openSettings(ActionEvent event) throws IOException {
         // Load the settings menu FXML document.
         root = FXMLLoader.load(getClass().getResource("SettingsMenu.fxml"));
-        // Get the stage by tracing the source of the click event -> scene -> stage.
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        // Create a scene with the loaded settings menu document.
-        scene = new Scene(root);
-        stage.setScene(scene);
-        // Center stage to the middle of the screen and prevent resizing.
-        stage.centerOnScreen();
-        stage.setResizable(false);
-        // Display the window.
+
+        MainMenuGrid.setConstraints(root, 2, 0);
+
+        MainMenuGrid.getChildren().add(root);
         stage.show();
+
     }
 }
