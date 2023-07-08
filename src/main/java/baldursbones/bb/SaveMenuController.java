e9173bc1596055cb72ab5d795cec262b48f8d00f
Braden Rogers <brogers42@my.bcit.ca>
2023-07-08 14:18:07 -0700

    Created save menu controller and added close window method + updated open save menu method in main menu.

---
+++ src/main/java/baldursbones/bb/SaveMenuController.java
@@ -0,0 +1,42 @@
+package baldursbones.bb;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+/**
+ * Settings Menu Controller.
+ *
+ * @author Braden Rogers
+ * @version Baldur's Bones v1.1
+ */
+public class SaveMenuController {
+
+    // The parent element the settings menu is displayed in.
+    private GridPane container;
+
+    // The layout element for the settings menu.
+    @FXML
+    private HBox saveGameMenu;
+
+    /** Removes the settings menu layout from the main menu and makes the buttons clickable again.
+     */
+    @FXML
+    public void closeSaveMenu() {
+        // Set main menu buttons to be clickable.
+        container.lookup("#openSettingsButton").setDisable(false);
+        container.lookup("#newGameButton").setDisable(false);
+        container.lookup("#savedGamesButton").setDisable(false);
+        container.lookup("#gameInfoButton").setDisable(false);
+        // Remove the settings menu from the main menu window.
+        container.getChildren().remove(saveGameMenu);
+    }
+
+    /** Takes the parent element that the layout will be displayed in and saves it.
+     *
+     * @param parentGrid The parent element of the settings menu layout.
+     */
+    public void getContainerElement(final GridPane parentGrid) {
+        container = parentGrid;
+    }
+}
