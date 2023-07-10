4ccda7e4b9c9897e87890933bc303b59e10e8862
Braden Rogers <brogers42@my.bcit.ca>
2023-07-10 13:28:58 -0700

    Added open and close methods for combat menu (end combat = close) and linked controller to FXML document.

---
+++ src/main/java/baldursbones/bb/GameCombatController.java
@@ -0,0 +1,44 @@
+package baldursbones.bb;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+/**
+ * Map Info Controller.
+ *
+ * @author Braden Rogers
+ * @version Baldur's Bones v1.1
+ */
+public class GameCombatController {
+
+    // The parent element the game combat menu is displayed in.
+    private GridPane container;
+
+    // The layout element for the game combat menu.
+    @FXML
+    private GridPane gameCombatGrid;
+
+    /**
+     * Removes the map info menu layout from the location menu and makes the buttons clickable again.
+     */
+    @FXML
+    public void closeGameCombatMenu() {
+        // Set location menu buttons to be clickable.
+        container.lookup("#locationFightButton").setDisable(false);
+        container.lookup("#locationViewStats").setDisable(false);
+        container.lookup("#locationViewMap").setDisable(false);
+        container.lookup("#endGameTest").setDisable(false);
+        // Remove the game info menu from the location menu window.
+        container.getChildren().remove(gameCombatGrid);
+    }
+
+    /**
+     * Takes the parent element that the layout will be displayed in and saves it.
+     *
+     * @param parentGrid The parent element of the character info menu layout
+     */
+    public void getContainerElement(final GridPane parentGrid) {
+        container = parentGrid;
+    }
+
+}
