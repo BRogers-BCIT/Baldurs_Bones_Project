7bccb11688ae2f3078bb2d69cb230b82a6f19455
Braden Rogers <brogers42@my.bcit.ca>
2023-07-10 10:27:41 -0700

    Added open saved game method to the save menu window (need to add game info passing) and added method link to load save button in FXML.

--- src/main/java/baldursbones/bb/SaveMenuController.java
+++ src/main/java/baldursbones/bb/SaveMenuController.java
@@ -1,8 +1,17 @@
 package baldursbones.bb;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Settings Menu Controller.
@@ -19,7 +28,8 @@ public class SaveMenuController {
     @FXML
     private HBox saveGameMenu;
 
-    /** Removes the settings menu layout from the main menu and makes the buttons clickable again.
+    /**
+     * Removes the settings menu layout from the main menu and makes the buttons clickable again.
      */
     @FXML
     public void closeSaveMenu() {
@@ -32,7 +42,29 @@ public class SaveMenuController {
         container.getChildren().remove(saveGameMenu);
     }
 
-    /** Takes the parent element that the layout will be displayed in and saves it.
+    /** Opens the Game Location window (main window for gameplay) in place of the current scene.
+     *
+     * @param event the event object created by clicking the load game button
+     * @throws IOException if the FXML document being loaded does not exist
+     */
+    @FXML
+    public void openGameWindow(final ActionEvent event) throws IOException {
+        // Load the Game Location menu FXML document.
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LocationMenu.fxml")));
+        // Get the stage by tracing the source of the click event -> scene -> stage.
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        // Create a scene with the loaded Game Location menu document.
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        // Center stage to the middle of the screen and prevent resizing.
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        // Display the window.
+        stage.show();
+    }
+
+    /**
+     * Takes the parent element that the layout will be displayed in and saves it.
      *
      * @param parentGrid The parent element of the settings menu layout.
      */
