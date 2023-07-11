391b8ce5c074679b9c7d42acf3c46ff627ef8348
Braden Rogers <brogers42@my.bcit.ca>
2023-07-11 11:40:36 -0700

    Created file reader to create objects for save file info from a text document and added a table population method.

--- src/main/java/baldursbones/bb/SaveMenuController.java
+++ src/main/java/baldursbones/bb/SaveMenuController.java
@@ -1,19 +1,30 @@
 package baldursbones.bb;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 /**
  * Saved Games Menu Controller.
@@ -21,7 +32,9 @@ import java.util.Objects;
  * @author Braden Rogers
  * @version Baldur's Bones v1.1
  */
-public class SaveMenuController {
+public class SaveMenuController implements Initializable {
+    // Static location of the save files to read from.
+    private static final File SAVE_FILES = new File("src/main/resources/baldursbones/bb/SavedGames.txt");
 
     // Define the amount of pixels to leave of the top of the screen for the anchor bar.
     private static final int ANCHOR_BAR_SIZE = 70;
@@ -32,6 +45,25 @@ public class SaveMenuController {
     // The layout element for the saved games menu.
     @FXML
     private HBox saveGameMenu;
+
+    // The table that the save files are contained in.
+    @FXML
+    private TableView<SaveFile> saveFileTable;
+
+    // The table column that contains the save file names.
+    @FXML
+    private TableColumn<SaveFile, String> nameColumn;
+
+    // The table column that contains the save file character names.
+    @FXML
+    private TableColumn<SaveFile, String> characterColumn;
+
+    // The table column that contains the save file times.
+    @FXML
+    private TableColumn<SaveFile, String> timeColumn;
+
+    // An array of the save file information.
+    private ObservableList<SaveFile> saveFiles;
 
     /**
      * Removes the saved games menu layout from the current menu and makes the buttons clickable again.
@@ -88,5 +120,39 @@ public class SaveMenuController {
      */
     public void getContainerElement(final GridPane parentGrid) {
         container = parentGrid;
+    }
+
+    // Populate the saved file table with the save file object info.
+    private void populateTable() {
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("fileName"));
+        characterColumn.setCellValueFactory(new PropertyValueFactory<>("characterName"));
+        timeColumn.setCellValueFactory(new PropertyValueFactory<>("saveTime"));
+        saveFileTable.setItems(saveFiles);
+    }
+
+    /**
+     * When the saved files menu is opened create an array of objects representing the save file data in memory.
+     *
+     * @param url            N/A
+     * @param resourceBundle N/A
+     */
+    @Override
+    public void initialize(final URL url, final ResourceBundle resourceBundle) {
+        saveFiles = FXCollections.observableArrayList();
+        // Try to read the save files from the save files text document.
+        try {
+            Scanner readSaves = new Scanner(SAVE_FILES);
+            while (readSaves.hasNext()) {
+                String fileName = readSaves.nextLine();
+                String characterName = readSaves.nextLine();
+                String saveTime = readSaves.nextLine();
+                String saveFile = readSaves.nextLine();
+                saveFiles.add(new SaveFile(fileName, characterName, saveTime, saveFile));
+            }
+        } catch (FileNotFoundException e) {
+            // Catch any errors reading the text file
+            throw new RuntimeException(e);
+        }
+        populateTable();
     }
 }
