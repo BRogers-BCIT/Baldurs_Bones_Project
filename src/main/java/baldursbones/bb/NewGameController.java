505f9368d08c479900b0a7b3a7dd138ac045bd36
Braden Rogers <brogers42@my.bcit.ca>
2023-07-12 11:19:41 -0700

    Created check for a character name and method to pass character name and tutorial skip boolean to game controller. Also connected FXML elements to controller.

--- src/main/java/baldursbones/bb/NewGameController.java
+++ src/main/java/baldursbones/bb/NewGameController.java
@@ -7,6 +7,8 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Screen;
@@ -23,15 +25,23 @@ import java.util.Objects;
  */
 public class NewGameController {
 
-    // Define the amount of pixels to leave of the top of the screen for the anchor bar.
+    // Constant: Define the amount of pixels to leave of the top of the screen for the anchor bar.
     private static final int ANCHOR_BAR_SIZE = 70;
 
-    // The parent element the new game menu is displayed in.
+    // FXML Element: The parent element the new game menu is displayed in.
     private GridPane container;
 
-    // The layout element for the new game menu.
+    // FXML Element: The layout element for the new game menu.
     @FXML
     private HBox newGameMenu;
+
+    //FXML Element: The text field for the user to enter their character name.
+    @FXML
+    private TextField characterName;
+
+    //FXML Element: The checkbox that allows players to disable the tutorial.
+    @FXML
+    private CheckBox disableTutorial;
 
     /**
      * Removes the new game menu layout from the main menu and makes the main menu buttons clickable again.
@@ -80,21 +90,44 @@ public class NewGameController {
     @FXML
     public void openGameWindow(final ActionEvent event) throws IOException {
         // Load the Game Location menu FXML document.
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LocationMenu.fxml")));
+        // Load the saved games menu FXML document into a root object.
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("SaveMenu.fxml"));
+        Parent root = loader.load();
         // Get the stage by tracing the source of the click event -> scene -> stage.
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        // Get the size of the users screen to set the size and with of the game window.
-        Rectangle2D userScreen = Screen.getPrimary().getBounds();
-        // Create a scene with the loaded Game Location menu document.
-        // Load the scene with the correct size and width found above.
-        Scene scene = new Scene(root, userScreen.getWidth(), userScreen.getHeight() - ANCHOR_BAR_SIZE);
-        stage.setScene(scene);
-        // Center stage to the middle of the screen, prevent resizing, and set title.
-        stage.centerOnScreen();
-        stage.setResizable(false);
-        stage.setTitle("Baldur's Bones");
-        // Display the window.
-        stage.show();
+        // Pass game info (character name and tutorial enabled) from window to game method.
+        if (getGameStartInfo(loader)) {
+            // Get the size of the users screen to set the size and with of the game window.
+            Rectangle2D userScreen = Screen.getPrimary().getBounds();
+            // Create a scene with the loaded Game Location menu document.
+            // Load the scene with the correct size and width found above.
+            Scene scene = new Scene(root, userScreen.getWidth(), userScreen.getHeight() - ANCHOR_BAR_SIZE);
+            stage.setScene(scene);
+            // Center stage to the middle of the screen, prevent resizing, and set title.
+            stage.centerOnScreen();
+            stage.setResizable(false);
+            stage.setTitle("Baldur's Bones");
+            // Display the window.
+            stage.show();
+        }
+    }
+
+    // Gets the character name from the text field and if the tutorial is disabled from the checkbox.
+    // Takes the controller for the new game window and passes the values to the controller.
+    // Prevents new game from being started without a character name.
+    // Limitations: ** CURRENTLY PRINTS FOR TESTING AND DOES NOT PASS VALUES **.
+    private boolean getGameStartInfo(final FXMLLoader loader) {
+        boolean startNewGame = true;
+        if (characterName.getText().equals("")) {
+            startNewGame = false;
+            // ** TEMP PRINT INSTEAD OF VISUAL OUTPUT **
+            System.out.println("No Character Name. Invalid Start.");
+        } else {
+            // ** TEMP PRINT INSTEAD OF PASS VARIABLE STATUS **
+            System.out.println("Character Name: " + characterName.getText() + ".");
+            System.out.println("Tutorial Disabled: " + disableTutorial.isSelected() + ".");
+        }
+        return startNewGame;
     }
 
     /**
