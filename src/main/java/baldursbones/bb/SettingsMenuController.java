750f4a979abe1666574dd346614a11fc7e22d056
Braden Rogers <brogers42@my.bcit.ca>
2023-07-12 10:59:44 -0700

    Added sound settings methods and connected the sound settings FXMl elements to the controller.

--- src/main/java/baldursbones/bb/SettingsMenuController.java
+++ src/main/java/baldursbones/bb/SettingsMenuController.java
@@ -3,15 +3,20 @@ package baldursbones.bb;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Settings Menu Controller.
@@ -19,14 +24,49 @@ import java.io.IOException;
  * @author Braden Rogers
  * @version Baldur's Bones v1.1
  */
-public class SettingsMenuController {
+public class SettingsMenuController implements Initializable {
 
-    // The parent element the settings menu is displayed in.
+    // Variable: Records if the sound settings menu is opened or closed
+    private boolean soundSettingsOpen;
+
+    // FXML Element: The parent element the settings menu is displayed in.
     private GridPane container;
 
-    // The layout element for the settings menu.
+    // FXML Element: The layout element for the settings menu.
     @FXML
     private HBox settingsMenu;
+
+    // FXML Element: The settings menu button to close the settings menu.
+    @FXML
+    private Button closeMenuButton;
+
+    // FXML Element: The settings menu button to open the save game menu.
+    @FXML
+    private Button openSaveMenuButton;
+
+    // FXML Element: The settings menu button to open the game info menu.
+    @FXML
+    private Button openGameInfobutton;
+
+    // FXML Element: The settings menu button to open the sound settings on the menu.
+    @FXML
+    private Button openSoundSettingsButton;
+
+    // FXML Element: The settings menu button to close the sound settings on the menu.
+    @FXML
+    private Button closeSoundSettingsButton;
+
+    // FXML Element: The settings menu button to close the quit the game.
+    @FXML
+    private Button quitGamebutton;
+
+    // FXML Element: The settings menu checkbox to control the music settings.
+    @FXML
+    private CheckBox musicCheckbox;
+
+    // FXML Element: The settings menu checkbox to control the sound effects settings.
+    @FXML
+    private CheckBox effectsCheckbox;
 
     /**
      * Removes the settings menu layout from the current menu and makes the buttons clickable again.
@@ -128,6 +168,47 @@ public class SettingsMenuController {
         popup.showAndWait();
     }
 
+    /**
+     * Update the visibility of buttons whenever the user open / closes the sound settings menu.
+     * ** Layout changes may also be needed in the future. **
+     */
+    @FXML
+    public void toggleSoundSettings() {
+        // Swap the visibility of the sound settings menu and regular settings menu.
+        if (!soundSettingsOpen) {
+            closeMenuButton.setVisible(false);
+            openSaveMenuButton.setVisible(false);
+            openGameInfobutton.setVisible(false);
+            openSoundSettingsButton.setVisible(false);
+            quitGamebutton.setVisible(false);
+            musicCheckbox.setVisible(true);
+            effectsCheckbox.setVisible(true);
+            closeSoundSettingsButton.setVisible(true);
+            soundSettingsOpen = true;
+        } else {
+            closeMenuButton.setVisible(true);
+            openSaveMenuButton.setVisible(true);
+            openGameInfobutton.setVisible(true);
+            openSoundSettingsButton.setVisible(true);
+            quitGamebutton.setVisible(true);
+            musicCheckbox.setVisible(false);
+            effectsCheckbox.setVisible(false);
+            closeSoundSettingsButton.setVisible(false);
+            soundSettingsOpen = false;
+        }
+    }
+
+    /**
+     * Gets the status of the sound menu and passes them to the game driver.
+     * ** Currently uses print lines for testing **
+     */
+    @FXML
+    public void updateSoundSettings() {
+        // ** TEMP PRINT INSTEAD OF PASS VARIABLE STATUS **
+        System.out.println("Music Disabled: " + musicCheckbox.isSelected() + ".");
+        System.out.println("Sound Effects Disabled: " + effectsCheckbox.isSelected() + ".");
+    }
+
 
     /**
      * Takes the parent element that the layout will be displayed in and saves it. Disables settings button on menu.
@@ -136,5 +217,16 @@ public class SettingsMenuController {
      */
     public void getContainerElement(final GridPane parentGrid) {
         container = parentGrid;
+    }
+
+    /**
+     * When the settings menu is first opened, set the sound settings variable to be false.
+     *
+     * @param url            N/A
+     * @param resourceBundle N/A
+     */
+    @Override
+    public void initialize(final URL url, final ResourceBundle resourceBundle) {
+        soundSettingsOpen = false;
     }
 }
