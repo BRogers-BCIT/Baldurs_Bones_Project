5ba0a4c4f4082a87f2eddabf9aaa70f15a509a4b
Braden Rogers <brogers42@my.bcit.ca>
2023-07-17 15:22:10 -0700

    Created the method to start the combat and pass the FXML element so it can update the Game Combat menu.

--- src/main/java/baldursbones/bb/GameCombatController.java
+++ src/main/java/baldursbones/bb/GameCombatController.java
@@ -18,6 +18,23 @@ public class GameCombatController {
     @FXML
     private GridPane gameCombatGrid;
 
+    // Game Object: The current Player object for display and combat class.
+    private Player player;
+
+    // Game Object: The current Enemy object for the combat class.
+    private Enemy enemy;
+
+    /**
+     * Create a new combat for the game combat menu and call the combat starter method.
+     *
+     * @param locationName a string used to represent the name of the location for the combat.
+     * @return an integer used to represent the outcome of the combat.
+     */
+    public int combatStarter(final String locationName) {
+        Combat combat = new Combat(player, enemy, gameCombatGrid);
+        return combat.combatStarter(locationName);
+    }
+
     /**
      * Removes the map info menu layout from the location menu and makes the buttons clickable again.
      */
@@ -35,10 +52,14 @@ public class GameCombatController {
     /**
      * Takes the parent element that the layout will be displayed in and saves it. Also re-enables Settings Menu.
      *
-     * @param parentGrid The parent element of the character info menu layout
+     * @param parentGrid      The parent element of the character info menu layout
+     * @param playerCharacter the Player object to be used in the Combat for this Combat Menu
+     * @param currentEnemy    the Enemy object to be used in the Combat for this Combat Menu
      */
-    public void getContainerElement(final GridPane parentGrid) {
+    public void getContainerElement(final GridPane parentGrid, final Player playerCharacter, final Enemy currentEnemy) {
         container = parentGrid;
+        player = playerCharacter;
+        enemy = currentEnemy;
         container.lookup("#openSettingsButton").setDisable(false);
     }
 
